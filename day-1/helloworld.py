ca58373314822ea6cfe212d3a1a3ec3322d8f179
Sarah <njagusarah@gmail.com>
2026-07-07 14:55:01 +0300

    Initial comit: Day 1 and Day 2

---
+++ day-1/helloworld.py
@@ -0,0 +1,27 @@
+print(4 + 6)
+print(10 - 6)
+print(4 * 6)
+print(10 % 5)
+print(10 / 2)
+print(5 ** 3)
+print(10 // 2)
+
+print("Sarah")
+print("Njagi")
+print("Kenya")
+print("I am enjoying 30 Days of Python")
+
+
+#Checking the data types of the variables
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Sarah'))
+print(type('Njagi'))
+print(type('Kenya'))
+
+#finding the euclidean distance between two points (2,3) and (10,8)
+import math
+print(math.sqrt((2 - 10) ** 2 + (3 - 8) ** 2))
---
+++ day-2/variables.py
@@ -0,0 +1,48 @@
+#Day 2: 30 Days of python programming
+first_name = "Sarah"
+last_name = "Njagi"
+full_name = "Sarah Njagi"
+country = "Kenya"
+city = "Nairobi"
+age = 24
+year = 2026
+is_married = False
+is_true = True
+is_light_on = False
+day, mood = 'Monday', 'Anxious'
+
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+print(len(first_name))
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+print(total)
+print(diff)
+print(product)
+print(division)
+print(remainder)
+
+area_of_circle = 3.14 * 30 * 30
+print(area_of_circle)
+
+radius = input('Radius: ')
+area = int(3.14) * int(radius) * int(radius)
+print(area)
